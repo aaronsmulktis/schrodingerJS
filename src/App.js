ee150d8becf388b146d696af3c66a19723fb5ad6
Aaron Smulktis <aaronsmulktis@gmail.com>
2023-02-03 14:21:55 -0800

    plot schrodingers equation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
+
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Plotly from 'plotly.js/dist/plotly-cartesian';
 
 function App() {
+
+  function d2psi(psi, x, h) {
+    let c = x + h;
+
+    return (psi(c) - 2 * psi(x) + psi(x - h)) / (h * h);
+  }
+
+  function schrodinger(psi, t, V, hbar, m) {
+    let newPsi = x => psi(x)
+    return -(hbar * hbar / (2 * m)) * d2psi(newPsi, 3, hbar) + V(m) * psi;
+  }
+  
+  // Use numerical methods such as finite differences to approximate the solution
+  // to the Schrödinger equation for a given potential energy function V(x) and initial wave function psi(x)
+  function solveSchrodinger(psi0, t, V, hbar, m, xmin, xmax, n) {
+    let h = (xmax - xmin) / n;
+    let x = xmin;
+    let psi = (s) => psi0(s);
+    let solution = [];
+    for (let i = 0; i < n; i++) {
+      solution.push(psi(x));
+      schrodinger(psi, t, V, hbar, m);
+      x += h;
+    }
+    return solution;
+  }
+
+  let psi0 = x => Math.exp(-x * x / 2); // Gaussian wave packet
+  let V = x => 0; // Free particle
+  let hbar = 1.0545718e-34;
+  let M = 9.10938356e-31;
+
+  let solution = solveSchrodinger(psi0, 0, V, hbar, M, 0.01, 10, 111);
+  
+  useEffect(() => {
+    console.log(solution)
+    Plotly.newPlot("plotGraph", [{
+      y: solution,
+      type: 'line'
+    }]);
+  }, [solution]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="plotGraph" />
     </div>
   );
 }
